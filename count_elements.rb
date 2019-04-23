44e3d3b541b988bb822967f8043f4fa300a37d13
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-23 20:12:17 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -5,11 +5,8 @@ def count_elements(array)
 
 count_elements = count_elements.to_h
 
-  count_elements.map do |key, value|
-    if key.count == 0
-      new_hash << key
-    else
-      new_hash << key.index +=1
+  count_elements.map do |element|
+    new_hash << element.count
 
 
 
